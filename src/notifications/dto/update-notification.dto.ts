f06f0618db3236241ee012846b598c2792481a9c
Joel <joestrell@alumnos.unex.es>
2025-11-12 18:59:10 +0100

    GG-97 - CA01: El usuario puede activar o desactivar tipos específicos de notificaciones (por ejemplo: lanzamientos, compras, seguidores, promociones). #time 2h #close tarea finalizada

--- src/notifications/dto/update-notification.dto.ts
+++ src/notifications/dto/update-notification.dto.ts
@@ -1,4 +1,26 @@
 // src/notifications/dto/update-notification.dto.ts
 import { Prisma } from '@prisma/client';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
-export class UpdateNotificationDto implements Prisma.NotificationUpdateInput {}
+
+export class UpdateNotificationDto implements Prisma.NotificationUpdateInput {
+    @IsOptional()
+    @IsString()
+    title?: string;
+
+    @IsOptional()
+    @IsString()
+    message?: string;
+
+    @IsOptional()
+    @IsString()
+    channel?: string;
+
+    @IsOptional()
+    @IsString()
+    userId?: string;
+
+    @IsOptional()
+    @IsBoolean()
+    read?: boolean;
+}
